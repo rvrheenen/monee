b13e4f253b7cce317cc26c0ec1b0f39c9cc73380
rvrheenen <rick.van.rheenen@gmail.com>
2017-06-19 13:12:17 +0200

    handle spaces

--- specialiser/analysis/analyse-single.sh
+++ specialiser/analysis/analyse-single.sh
@@ -16,43 +16,43 @@ SCRIPT_DIR=`dirname "$DIR"`
 # SCRIPT_DIR=`dirname "$(echo `readlink -fn $0` | sed 's/ /\\ /g')"`
 
 echo counts
-bash ${SCRIPT_DIR}/counts.sh --iterations ${FLAGS_iterations}
+bash "${SCRIPT_DIR}/counts.sh" --iterations ${FLAGS_iterations}
 
 echo pucks
-bash ${SCRIPT_DIR}/pucks.sh
+bash "${SCRIPT_DIR}/pucks.sh"
 
 echo count-offspring
-bash ${SCRIPT_DIR}/count-offspring.sh
+bash "${SCRIPT_DIR}/count-offspring.sh"
 
 echo analyse-pressure
-bash ${SCRIPT_DIR}/analyse-pressure.sh
+bash "${SCRIPT_DIR}/analyse-pressure.sh"
 
 echo ages
-bash ${SCRIPT_DIR}/ages.sh
+bash "${SCRIPT_DIR}/ages.sh"
 
 echo pucks-vs-age
-bash ${SCRIPT_DIR}/pucks-vs-age.sh
+bash "${SCRIPT_DIR}/pucks-vs-age.sh"
 
 # echo calc_ratios
 # bash ${SCRIPT_DIR}/calc_ratios.sh --iterations ${FLAGS_iterations}
 
-bash ${SCRIPT_DIR}/count-inseminations.sh --iterations ${FLAGS_iterations}
+bash "${SCRIPT_DIR}/count-inseminations.sh --iterations ${FLAGS_iterations}"
 
 echo plot plot-pucks-ratio
-gnuplot ${SCRIPT_DIR}/plot-pucks-ratio
+gnuplot "${SCRIPT_DIR}/plot-pucks-ratio"
 
 echo plot plot-pucks-counts
-gnuplot ${SCRIPT_DIR}/plot-pucks-counts
-gnuplot ${SCRIPT_DIR}/plot-pucks-counts-total
+gnuplot "${SCRIPT_DIR}/plot-pucks-counts"
+gnuplot "${SCRIPT_DIR}/plot-pucks-counts-total"
 
 echo plot plot-histograms
-gnuplot  -e "set terminal pngcairo enhanced font 'Helvetica,11' size 1024, 1024;set output 'greenratio-histogram.png'" ${SCRIPT_DIR}/plot-histograms.gpl
+gnuplot -e "set terminal pngcairo enhanced font 'Helvetica,11' size 1024, 1024;set output 'greenratio-histogram.png'" "${SCRIPT_DIR}/plot-histograms.gpl"
 
 echo plot plot-hexbin-puckcounts
-awk '999000 < $1 {print $3,$4}' *.collected | gnuplot -e "set output 'hexbin-puckcounts.png'" ${SCRIPT_DIR}/plot-hexbin-puckcounts.gnuplot
+awk '999000 < $1 {print $3,$4}' *.collected | gnuplot -e "set output 'hexbin-puckcounts.png'" "${SCRIPT_DIR}/plot-hexbin-puckcounts.gnuplot"
 
 echo plot-inseminations
-gnuplot ${SCRIPT_DIR}/plot-inseminations
+gnuplot "${SCRIPT_DIR}/plot-inseminations"
 
 # for i in *png
 # do 
